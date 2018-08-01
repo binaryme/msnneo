6da9e7bac39ff3b3c8a3598d69f258a21c928ae8
Israel <nubestra@gmail.com>
2018-07-31 22:28:52 -0500

    Interfaz de mensajes primer iteración
    Eliminé dashboard para poner el componente "mensajes"

--- src/app/components/mensajes/mensajes.component.spec.ts
+++ src/app/components/mensajes/mensajes.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DashboardComponent } from './dashboard.component';
+import { MensajesComponent } from './mensajes.component';
 
-describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+describe('MensajesComponent', () => {
+  let component: MensajesComponent;
+  let fixture: ComponentFixture<MensajesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ MensajesComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
+    fixture = TestBed.createComponent(MensajesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
